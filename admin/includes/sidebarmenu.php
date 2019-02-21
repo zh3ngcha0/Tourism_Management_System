adf08959c4b807ab9222367436a2f88e90413ba7
zh3ngcha0 <zh3ngcha0@gmail.com>
2019-02-21 22:18:42 +0800

    add features

--- admin/includes/sidebarmenu.php
+++ admin/includes/sidebarmenu.php
@@ -5,12 +5,23 @@
 						<div style="border-top:1px ridge rgba(255, 255, 255, 0.15)"></div>
                            <div class="menu">
 									<ul id="menu" >
-										<li><a href="dashboard.php"><i class="fa fa-tachometer"></i> <span>管理菜单</span><div class="clearfix"></div></a></li>
+										<li><a href="dashboard.php"><i class="fa fa-tachometer"></i> <span>首页</span><div class="clearfix"></div></a></li>
 										
-									 <li id="menu-academico" ><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i><span>旅游菜单</span> <span class="fa fa-angle-right" style="float: right"></span><div class="clearfix"></div></a>
+									 <li id="menu-academico" ><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i><span>产品</span> <span class="fa fa-angle-right" style="float: right"></span><div class="clearfix"></div></a>
 										   <ul id="menu-academico-sub" >
-										   <li id="menu-academico-avaliacoes" ><a href="create-package.php">添加套餐</a></li>
-											<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">管理套餐</a></li>
+										   <li id="menu-academico-avaliacoes" ><a href="create-package.php">产品管理</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">产品分类设置</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">所有产品列表</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">周边旅游</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">国内旅游</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">出境旅游</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">单项服务</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">热门产品</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">特价产品</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">旅游新品</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">待审批产品</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">已审批产品</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">被拒绝产品</a></li>
 										  </ul>
 										</li>
 									<li id="menu-academico" ><a href="manage-users.php"><i class="fa fa-users" aria-hidden="true"></i><span>管理用户</span><div class="clearfix"></div></a></li>
